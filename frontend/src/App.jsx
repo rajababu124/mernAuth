6f355b0ce9f898c2a15aa4b869172b711a6e9d32
Raja Babu <raja.babu@progressive.in>
2024-07-27 12:33:52 +0530

    hey added some pages and routes for them

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
-
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/SignUp'
+import Profile from './Components/Profile/Profile'
 function App() {
 
   return (
     <>
-<div className="text-green-500">
-Hello World !!
-</div>
+      <BrowserRouter >
+       <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
+       </Routes>
+      </BrowserRouter>
     </>
   )
 }
